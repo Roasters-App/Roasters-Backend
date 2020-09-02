655d1692b89d5ff9d3b24212792d6b62cabfaffe
getchan <9511chn@gmail.com>
2020-09-02 14:07:08 +0900

    레포지토리 생성

--- hello.py
+++ hello.py
@@ -1,14 +1,19 @@
 from sanic import Sanic
+from sanic.log import logger
 from sanic.response import json
 
 app = Sanic(__name__)
-app.config.from_envvar("SETTING_PATH")
+app.config.from_envvar("CONFIG_FILE")
 
 
 @app.route("/")
 async def test(request):
+    logger.info("Request arrived")
     return json({"hello": "world"})
 
 
 if __name__ == "__main__":
-    app.run(app.config.SANIC_HOST, app.config.SANIC_PORT)
+    if app.config.PRODUCTION:
+        app.run(access_log=True)
+    else:
+        app.run(debug=True, access_log=True)
